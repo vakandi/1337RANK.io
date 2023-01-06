68470555c801e18f0361d795f30d8189ebc64df2
vakandi <wael.bousfira@protonmail.com>
2023-01-06 08:14:52 +0100

    update

--- script/create_json.sh
+++ script/create_json.sh
@@ -1,8 +1,9 @@
 var=1
 i=$1
-LOGIN="$(cat data/rank_clean.txt | awk  '{print $2}' | head -$i | tail -1)"
-LEVEL="$(cat data/rank_clean.txt | awk  '{print $3}' | head -$i | tail -1)"
-COALITION="$(cat data/rank_clean.txt | awk  '{print $4}' | head -$i | tail -1)"
+RANKFILE=data/rank_with_coalitions.txt
+LOGIN="$(cat $RANKFILE| awk  '{print $2}' | head -$i | tail -1)"
+LEVEL="$(cat $RANKFILE | awk  '{print $3}' | head -$i | tail -1)"
+COALITION="$(cat $RANKFILE | awk  '{print $4}' | head -$i | tail -1)"
 VARTMP=1
 VARLOGIN=1
 JSON_FMT='"%s": [{"login": "%s", "level": "%s", "coalition": "%s"}], '
--- script/make_simple_rank.sh
+++ script/make_simple_rank.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 var=1
-echo "" > data/rank_clean.txt
+RANKFILE=data/rank_with_coalitions.txt
+echo "" > $RANKFILE
 echo "$(cat data/rank.txt | rev | cut -c4- | rev)" > data/rank.tmp
 while [ $var -lt 200 ]
 do
 	login1337="$(cat data/rank.tmp | awk '{print $2}' | head -n$var | tail -n1)"
 	COALITION="$(cat data/coalitions.txt | grep $login1337 |  awk '{print $4}' | cut -c2- | rev | cut -c3- | rev)"
-	echo "$(cat data/rank.tmp | head -n$var | tail -n1 | sed "s/$/$COALITION/g")" >> data/rank_clean.txt
+	echo "$(cat data/rank.tmp | head -n$var | tail -n1 | sed "s/$/$COALITION/g")" >> $RANKFILE
 	echo "Adding $COALITION to $login1337 into the line $var"
 	var=$((var+1))
 done
-echo "$(cat data/rank_clean.txt | sed '1d')" > data/rank_clean.txt
+echo "$(cat $RANKFILE | sed '1d')" > $RANKFILE
