7be179aca322fb0bcd84426abe4927c1e1f6ba74
vakandi <106479214+vakandi@users.noreply.github.com>
2023-01-06 12:37:34 +0100

    Delete script directory

--- script/useful/get_coalition_of_every_students.sh
+++
@@ -1,8 +0,0 @@
-var=0
-echo "" > ../data/coalitions.txt
-while [ $var -lt 200 ]
-do
-	var=$((var+1))
-	python3 get_coalitions_of_onestudent.py $(cat rank.txt | awk '{print $2}' | head -n$var | tail -n1) >> ../data/coalitions.txt
-	echo "LOOP $var"
-done
