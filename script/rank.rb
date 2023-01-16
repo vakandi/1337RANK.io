152e89431dbf33638b22e4e2310ef0856d055676
wbousfi <wael.bousfira@protonmail.com>
2023-01-16 01:45:03 +0100

    update19479

--- script/rank.rb
+++ script/rank.rb
@@ -34,8 +34,10 @@ def coordination
   users = fetch_users(token, 1)
   sleep(0.5)
   users += fetch_users(token, 2)
+  sleep(0.5)
+  users += fetch_users(token, 3)
   users = users.sort_by { |k| k["created_at"] }.reverse
-  pats = users[0...200]
+  pats = users[0...203]
   pats = pats.sort_by { |k| k["level"] }.reverse
   pats.each.with_index(1) do |user, idx|
     puts "#{idx}  #{user["user"]["login"]} #{user["level"]}    "
