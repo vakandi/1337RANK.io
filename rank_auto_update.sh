b786951c040f164a1d8ebf917a88fced3c4376a1
wbousfi <wael.bousfira@protonmail.com>
2023-02-11 23:05:15 +0100

    update6083

--- rank_auto_update.sh
+++ rank_auto_update.sh
@@ -2,6 +2,7 @@
 git pull
 #Get Levels of students from api
 ruby script/rank.rb > data/rank_without_coalitions.txt
+#ruby script/rank_without_bot_fix.rb > data/rank_without_coalitions.txt
 
 touch data/data-tmp.json
 touch data/format_final.json
