9bf83b12a9eb8e54efd415ac347c7afca48e4c6b
wbousfi <wael.bousfira@protonmail.com>
2023-02-13 22:30:35 +0100

    update7504

--- rank_auto_update.sh
+++ rank_auto_update.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 git pull
 #Get Levels of students from api
-ruby script/rank.rb > data/rank_without_coalitions.txt
-#ruby script/rank_without_bot_fix.rb > data/rank_without_coalitions.txt
+#ruby script/rank.rb > data/rank_without_coalitions.txt
+ruby script/rank_without_bot_fix.rb > data/rank_without_coalitions.txt
 
 #Fixing not existing files error on ruby
 touch data/data-tmp.json
 touch data/format_final.json
 
 #Make rank
-#ruby script/update.rb
+ruby script/update_rank_without_bot_fix.rb
 
 #Shell Version for the backend
 #sh script/make_rank.sh
